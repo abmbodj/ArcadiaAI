6193f7a39654673bd5aaa583a7da7a01a32d01bc
-insert name here- <104537575+eva-akselrad@users.noreply.github.com>
2025-10-22 10:24:45 -0400

    Refactor project structure and update Flask app
    Moved static assets and templates to 'src/static' and 'src/templates' directories for better organization. Updated Flask app in app.py to serve new routes and use the reorganized template/static paths. Adjusted HTML files to reference new static paths and added favicon support.

--- src/app.py
+++ src/app.py
@@ -1,4 +1,33 @@
+import os
+import sys
+import flask as fk
+proj_root = os.path.dirname(__file__)          # project root if app.py is there
+src_dir = os.path.join(proj_root, "src")
+sys.path.insert(0, src_dir)
 from lib import GemInterface
+
 gemini = GemInterface.AiInterface()
 
-print(gemini.Archie("What majors does Arcadia University offer?"))
+
+
+app = fk.Flask(__name__)
+
+@app.route("/ask", methods=["POST"])
+def ask():
+    user_input = fk.request.json.get("question")
+    response = gemini.Archie(user_input)
+    return fk.jsonify({"response": response}) 
+
+@app.route("/", methods=["GET"])
+def home():
+    indextemplate = fk.render_template("index.html")
+    return indextemplate
+
+@app.route("/chats", methods=["GET"])
+def chats():
+    chatstemplate = fk.render_template("chats.html")
+    return chatstemplate
+
+if __name__ == "__main__":
+    print("Working Directory:", os.getcwd())
+    app.run(debug=True)
